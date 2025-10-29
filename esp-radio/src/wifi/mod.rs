7f18a252b1738c816de0e1ea7a5b9a9c88ceec4b
Dane Slattery <dane@polarmonitoring.com>
2025-10-29 13:41:52 +0200

    use the use

--- esp-radio/src/wifi/mod.rs
+++ esp-radio/src/wifi/mod.rs
@@ -3152,7 +3152,7 @@ impl WifiController<'_> {
         if self.mode()?.is_sta() {
             let mut record: MaybeUninit<include::wifi_ap_record_t> = MaybeUninit::uninit();
             let record = unsafe { MaybeUninit::assume_init_mut(&mut record) };
-            esp_wifi_result!(unsafe { include::esp_wifi_sta_get_ap_info(record) })?;
+            esp_wifi_result!(unsafe { esp_wifi_sta_get_ap_info(record) })?;
 
             let ap_info = convert_ap_info(record);
             Ok(ap_info)
